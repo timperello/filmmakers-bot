1c2777cf95770eb6cb0b72a61024026f6be47032
Timo <74350893+timperello@users.noreply.github.com>
2026-03-26 00:59:08 +0900

    Update script.py

--- script.py
+++ script.py
@@ -66,7 +66,7 @@ if last_id:
         new_posts = all_posts[:index]
     else:
         print("⚠️ last_id non trouvé → fallback")
-        new_posts = all_posts[:5]
+        new_posts = all_posts
 
 else:
     print("First run → init seulement")
@@ -74,15 +74,18 @@ else:
 
 # 🔹 Envoyer notifications
 if new_posts:
-    print(f"{len(new_posts)} nouveaux posts 🚀")
+    # ✅ Message summary unique
+    summary_msg = f"🚀 {len(new_posts)} nouvelles offres sur Filmmakers !"
+    requests.post(WEBHOOK, json={"content": summary_msg})
+    print(summary_msg)
 
+    # ✅ Ensuite on loop pour les détails
     for post_id, title, link, post_time in reversed(new_posts):
         message = (
-            f"  \n\n"
-            f"🎬 **New casting**\n\n"
-            f"📝 {title}\n\n"
-            f"🕒 {post_time}\n\n"
-            f"🔗 {link}\n"
+            f"🎬 **New casting**\n"
+            f"📝 {title}\n"
+            f"🕒 {post_time}\n"
+            f"🔗 {link}"
         )
         requests.post(WEBHOOK, json={"content": message})
 
